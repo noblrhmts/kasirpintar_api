4501ddc10992c5d044137390953e70368a85adcf
Nobel <nobelrahmat54@gmail.com>
2024-11-12 19:01:00 +0700

    Praktikum Kasir Pintar API

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        // Validasi data
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        try {
+            // Buat pengguna baru
+            $user = User::create([
+                'name' => $validatedData['name'],
+                'email' => $validatedData['email'],
+                'password' => bcrypt($validatedData['password']),
+            ]);
+
+            // Buat token untuk pengguna
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+            ]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Registrasi gagal, silakan coba lagi.'], 500);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        // Validasi kredensial
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+
+        try {
+            // Cari pengguna berdasarkan email
+            $user = User::where('email', $request['email'])->firstOrFail();
+
+            // Buat token untuk pengguna
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+            ]);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Login gagal, silakan coba lagi.'], 500);
+        }
+    }
+
+}
